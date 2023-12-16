d791965b9eada6a65769c269bc9e5fbfa939f8fc
panchal-ayuj <panchal.kumar@accolitedigital.com>
2023-12-16 20:50:06 +0530

    First commit

---
+++ src/main/java/com/example/part2/Part2Application.java
@@ -0,0 +1,14 @@
+package com.example.part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Part2Application.class, args);
+	}
+
+}
